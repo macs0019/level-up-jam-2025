2448649d77b3b40a4b8d3ef4c50499adc5f5c936
macs0021 <carrionmarcoa@gmail.com>
2025-07-13 00:22:14 +0200

    Solved remember container bug

--- Assets/Scripts/NamerManager.cs
+++ Assets/Scripts/NamerManager.cs
@@ -353,7 +353,7 @@ public class NamerManager : MonoBehaviour
 
         if (Keyboard.current != null && Keyboard.current.enterKey.wasPressedThisFrame && canSkipRememberingView)
         {
-            rememberContainer.SetActive(false);  
+            //rememberContainer.SetActive(false);  
             rememberContainer.GetComponent<RectTransform>().DOAnchorPosX(800f, 0.5f);
             canSkipRememberingView = false; // Resetear la variable para evitar saltar la vista de recuerdos múltiples veces
             OnAllInputsFilled(); // Llamar a la lógica de finalizar después de desactivar el contenedor
